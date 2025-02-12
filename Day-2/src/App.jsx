5b0b7075470efc9222de4d9ebc08a620262492b4
Akshaay BS <bsakshaay5@gmail.com>
2025-02-12 10:09:02 +0530

    Add Cars

--- Day-2/src/App.jsx
+++ Day-2/src/App.jsx
@@ -1,13 +1,37 @@
-import Greetings from "./Greetings";
+// import Greetings from "./Greetings";
 
-export default function App(){
+// export default function App(){
+//   return (
+//     <>
+//       <h1>
+//         <marquee >Welcome to REACT
+//         </marquee>
+//       </h1>
+//       <Greetings />
+//     </>
+//   )
+// }
+
+
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import CarList from "./cars/CarList";
+import CarCreate from "./cars/CarCreate";
+import CarView from "./cars/CarView";
+
+function App() {
   return (
     <>
-      <h1>
-        <marquee >Welcome to REACT
-        </marquee>
-      </h1>
-      <Greetings />
+      <BrowserRouter>
+        <Routes>
+            <Route path="" element={<CarList/>}/>
+            <Route path="/car/list" element={<CarList/>}/>
+            <Route path="/car/create" element={<CarCreate/>}/>
+            <Route path="/car/view" element={<CarView/>}/>
+        </Routes>
+      
+      </BrowserRouter>
     </>
   )
 }
+
+export default App
